62833f8c3067cabd8908ed0f5b32f6bf1c129808
Sibananda <sibanandadora443@gmail.com>
2025-11-03 08:24:10 +0530

    toolnode and tools_condition

---
+++ 1.LangGraphBasics/7.ToolNodeRouting.py
@@ -0,0 +1,68 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage,AIMessage,AnyMessage
+from typing import TypedDict,Annotated
+from langgraph.graph.message import add_messages
+from IPython.display import display,Image
+from langgraph.graph import StateGraph,START,END
+from pprint import pprint
+from langgraph.prebuilt import ToolNode,tools_condition
+
+load_dotenv()
+os.environ['GOOGLE_API_KEY']=os.getenv("GOOGLE_API_KEY")
+llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+# print(llm.invoke("hey there "))
+
+def subtract(a:int,b:int)->int:
+    ''' 
+    Subtract a and b
+    a(int): first int 
+    b(int): second int 
+
+    returns:
+    int
+    '''
+    return a-b
+
+llm_with_tools=llm.bind_tools([subtract])
+
+# x=llm_with_tools.invoke("subtract 5 from 10")
+# x=llm_with_tools.invoke("subtract 5 from 10")
+
+# print(x)
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage],add_messages]
+
+initial_msg=[AIMessage(content=f"Hey there!What can I do for you today ?",name="LLMModel")]
+initial_msg.append(HumanMessage(content=f"I want to learn GenAI but I am confused, in what language should I start ?",name="Sibananda"))
+ai_msg=[AIMessage(content=f"What languages are you familiar with ?",name="LLMModel")]
+
+add_messages(initial_msg,ai_msg)
+
+def llm_tool(state:State):
+    return {"messages":[llm_with_tools.invoke(state["messages"])]}
+
+tools_=[subtract]
+builder=StateGraph(State)
+builder.add_node("llm_tool",llm_tool)
+builder.add_node("tools",ToolNode(tools_))
+builder.add_edge(START,"llm_tool")
+builder.add_conditional_edges("llm_tool",tools_condition)
+builder.add_edge("tools",END)
+graph_builder=builder.compile()
+display(Image(graph_builder.get_graph().draw_mermaid_png()))
+
+#tool calling✅
+y=graph_builder.invoke({"messages":"5-2"})
+for x in y["messages"]:
+    print(x.pretty_print())
+
+
+#without tool call
+z=graph_builder.invoke({"messages":"tell me about linear regression"})
+for zx in z["messages"]:
+    print(zx.pretty_print())
+
+    
